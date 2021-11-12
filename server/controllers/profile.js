f4eae4d8db60322069090a8cbd52a5a236716d68
TetrisXaddict <94081349+TetrisXaddict@users.noreply.github.com>
2021-11-12 15:03:14 -0600

    Finished controller for newProfile and created controller and route for updateProfile

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,6 +1,6 @@
 export const getAllProfiles = async (req, res) => {
   try {
-    const foundProfiles = profile.find();
+    const foundProfiles = await profile.find();
 
     res.status(200).json(foundProfiles);
   } catch (error) {
@@ -15,6 +15,19 @@ export const getProfile = (req, res) => {
   } catch (error) {}
 };
 
-export const createProfile = (req, res) => {
-  res.send("Router is working");
+export const createProfile = async (req, res) => {
+  const profileVar = req.body;
+
+  const newProfile = new profile(profileVar);
+
+  try {
+    await newProfile.save();
+    res.status(201).json(newProfile);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const updateProfile = (req, res) => {
+  //todo
 };
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,6 +3,7 @@ import {
   getAllProfiles,
   getProfile,
   createProfile,
+  updateProfile,
 } from "../controllers/profile.js";
 import student from "../models/profile.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/", getAllProfiles); //get all profiles
 router.get("/", getProfile); // get singular profile
 router.post("/", createProfile); //create a profile
+router.put("/", updateProfile); //update existing profile
 
 export default router;
