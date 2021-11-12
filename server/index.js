b1bc23f932d7c54b36a2749b79effb245802b386
TetrisXaddict <94081349+TetrisXaddict@users.noreply.github.com>
2021-11-12 11:00:29 -0600

    Added 3 app.use cases in index.js
    added bodyparsers for .json and urlencoded.
    
    add a line for the app to use cors.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
-import Express from "express";
-import bodyParser from "body-parser";
-import Mongoose from "mongoose";
+import express from "express";
+//import bodyParser from "body-parser"; //"Installed for tutorial purposes. Learned that it was depreciated"
+import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "20mb", extended: true }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
