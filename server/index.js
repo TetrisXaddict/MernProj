b0420e9f8d2568916fbf927a3569a6a7058f8209
TetrisXaddict <94081349+TetrisXaddict@users.noreply.github.com>
2021-11-12 11:38:47 -0600

    added mongo db url to index.js as a const

--- server/index.js
+++ server/index.js
@@ -9,3 +9,6 @@ app.use(express.json({ limit: "20mb", extended: true }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://Admin:<password>@cluster0.oecrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
