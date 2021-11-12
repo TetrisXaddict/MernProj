ce8b87cbb71f0e7133175e205aa8a9f522827484
TetrisXaddict <94081349+TetrisXaddict@users.noreply.github.com>
2021-11-12 12:28:00 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,3 +12,19 @@ app.use(cors());
 
 const CONNECTION_URL =
   "mongodb+srv://User_1:<NPw8K9QM4kcNPYuL>@cluster0.oecrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log("Connection is established and running on port:" + { PORT })
+    )
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
